1d5ff96fe92ae8b06c4c2eaaaf5b237f114fc71b
GeraninDmitry <GeraninDmitry@mail.ru>
2021-06-30 11:15:35 +0300

    Добавлен класс для первоначального заполнения db, добавлен Seq

---
+++ FilmsCatalog/Helpers/DbFillHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmsCatalog.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilmsCatalog.Helpers
+{
+    public static class DbFillHelpers
+    {
+        const string WwwRootDirectory = "wwwroot//";
+
+        public static IWebHost FillDb(this IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                using (var context = services.GetRequiredService<ApplicationDbContext>())
+                {
+                    try
+                    {
+                        context.Database.EnsureCreated();
+
+                        //TODO: Добавить заполнение db.
+
+                    }
+                    catch (Exception ex)
+                    {
+                        // TODO: добавить лог.
+                    }
+                }
+            }
+
+            return webHost;
+        }
+
+
+    }
+}
--- FilmsCatalog/Program.cs
+++ FilmsCatalog/Program.cs
@@ -4,23 +4,36 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmsCatalog.Helpers;
 
 namespace FilmsCatalog
 {
     public class Program
     {
+
         public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+		{
+			var webHost = new WebHostBuilder().UseIIS().UseContentRoot(Directory.GetCurrentDirectory())
+				.ConfigureAppConfiguration((hostingContext, config) =>
+				{
+					config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+					config.AddEnvironmentVariables();
+				}).ConfigureLogging((builderContext, loggingBuilder) =>
+				{
+					loggingBuilder.AddSeq(builderContext.Configuration.GetSection("Seq"));
+					loggingBuilder.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+				}).UseStartup<Startup>().Build()
+#if DEBUG
+				.FillDb();
+#else
+            ;
+#endif
+
+            webHost.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
